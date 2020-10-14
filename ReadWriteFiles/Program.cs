14919d478d1f875aede4aadadd8938ae7d47a582
Marcus <Marcus@DATOR>
2020-10-14 09:27:03 +0200

    Add project files.

---
+++ ReadWriteFiles/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ReadWriteTextFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WriteToFile("persons.txt", "Marcus, Är ,Bäst,God,Like");
+        }
+
+        private static void WriteToFile(string filename,string text)
+        {
+            var lines = new List<string>();
+            lines.Add(text);
+
+            File.AppendAllLines(@$"D:\Files\{filename}", lines);
+
+            //File.WriteAllLines(@$"D:\Files\{filename}", lines);
+
+        }
+
+    }
+}
