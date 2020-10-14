b0f42a594250ccdb74c8175f1564c676ca94c213
Marcus <Marcus@DATOR>
2020-10-14 09:41:36 +0200

    Read

--- ReadWriteFiles/Program.cs
+++ ReadWriteFiles/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace ReadWriteTextFile
 {
@@ -9,7 +10,8 @@ namespace ReadWriteTextFile
         static void Main(string[] args)
         {
 
-            WriteToFile("persons.txt", "Marcus, Är ,Bäst,God,Like");
+            WriteToFile("persons.txt", "Marcus, Är ,Bäst,GodLike");
+            ReadFromFile("persons.txt");
         }
 
         private static void WriteToFile(string filename,string text)
@@ -17,9 +19,27 @@ namespace ReadWriteTextFile
             var lines = new List<string>();
             lines.Add(text);
 
+            //File.WriteAllLines(@$"D:\Files\{filename}", lines);
             File.AppendAllLines(@$"D:\Files\{filename}", lines);
 
-            //File.WriteAllLines(@$"D:\Files\{filename}", lines);
+
+        }
+
+        private static void ReadFromFile(string filename)
+        {
+            Console.WriteLine("Using List");
+            var lines = File.ReadAllLines(@$"D:\Files\{filename}").ToList();
+            foreach(var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.WriteLine("Using Array");
+            var lines2 = File.ReadAllLines(@$"D:\Files\{filename}");
+            foreach (var line in lines2)
+            {
+                Console.WriteLine(line);
+            }
 
         }
 
