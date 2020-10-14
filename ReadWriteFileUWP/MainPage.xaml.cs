996f0fe52f5ae0dc7de86674ee88019723a0157e
Marcus <Marcus@DATOR>
2020-10-14 14:28:53 +0200

    UWP

---
+++ ReadWriteFileUWP/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ReadWriteFileUWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            //Task.Run(() => CreateFileAsync());
+            CreateFileAsync().GetAwaiter();
+        }
+
+        private async Task CreateFileAsync()
+        {
+            //StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            StorageFolder storageFolder = KnownFolders.DocumentsLibrary;
+
+
+            StorageFile storageFile = await storageFolder.CreateFileAsync("Lok.txt",CreationCollisionOption.ReplaceExisting);
+        }
+    }
+}
